53ceab371b3891fca4c99968d8cc19229dfcddbd
wwofford <weston@wofford.app>
2019-08-18 23:32:56 -0400

    Removed unused fontawesome icon

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import App from './App.vue'
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFlag, faCat, faStopwatch, faUndo, faCog, faPaw } from '@fortawesome/free-solid-svg-icons'
+import { faCat, faStopwatch, faUndo, faCog, faPaw } from '@fortawesome/free-solid-svg-icons'
 
-library.add( faFlag, faCat, faStopwatch, faUndo, faCog, faPaw )
+library.add( faCat, faStopwatch, faUndo, faCog, faPaw )
 
 Vue.config.productionTip = false
 
